e632ecfb58f56bdaf25f2ab36096b0ca2d835763
= <palach782@gmail.com>
2025-05-23 12:28:04 +0300

    Category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
 
+    name = models.CharField(max_length=100, unique=True, verbose_name='Назва категорії')
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+        ordering = ['name']
+
+    def __iter__(self):
+        return self
+
+    def __str__(self):
+        return self.name
