ae04d424dbb0cd31ce7de40540d491704b8966f3
Hryhorenko-Yeluzaveta <lizokgrugorenchuk@gmail.com>
2025-05-23 12:33:59 +0300

    Added Recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -14,3 +14,26 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    instructions = models.TextField()
+    ingredients = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='recipes',
+        verbose_name='Категорія'
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепти'
+
+    def __str__(self):
+        return self.title
